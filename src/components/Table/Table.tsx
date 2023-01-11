823198fdff9a05302ef1c871880544a5eac7abc7
Utkarsh Saxena <utkarsh.saxena@unicommerce.com>
2023-01-12 00:39:45 +0530

    Plug shipping list API

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,29 +1,53 @@
 import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Input, useDisclosure } from "@chakra-ui/react";
 import { useVirtualizer } from "@tanstack/react-virtual";
-import { Fragment, useRef, useState } from "react"
+import { Fragment, useEffect, useRef, useState } from "react"
 
 export default function Table() {
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const [shippingList, setShippingList] = useState<any[]>([]);
     const [shipmentDetailIndex, setShipmentDetailIndex] = useState<number>(0);
+
+    useEffect(() => {
+        async function fetchShipmentList() {
+            try {
+                const res = await fetch("https://qa-unishipper.unicommerce.com/shipper/api/tracking-list", {
+                    method: "POST",
+                    headers: {
+                        'APP-KEY': '#$%^SK&SNLSH*^%SF'
+                    },
+                    body: JSON.stringify({})
+                });
+
+                if (!res.ok) throw new Error(res.statusText);
+
+                const data = await res.json();
+                setShippingList(data.result.tracking_records.map((row: any) => {
+                    return [
+                        `AWB: ${row.tracking_number}, Courier: ${row.shipping_source_code}`,
+                        `${row.order_number}`,
+                        `${row.customer_name}, ${row.customer_phone}`,
+                        `${row.shipping_package_code}`,
+                        `${row.facility_code}`,
+                        `${row.current_wismo_display_status}`,
+                        `${row.order_datetime}`,
+                        `${row.dispatch_datetime}`,
+                        `${row.expected_delivered_datetime}`,
+                        `${row.delivered_datetime}`,
+                        `${row.no_of_items}`,
+                    ]
+                }))
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        fetchShipmentList();
+    }, [])
 
     const showShipmentDetails = (index: number) => {
         setShipmentDetailIndex(index);
         onOpen();
     }
-
-    const data = Array(1000).fill([
-        'AWB: 3108518186, Courier: ECOM',
-        '4695400513715',
-        'Raghav, 8171505570',
-        'SP-BHI-FY23-175840',
-        'ARIHANT',
-        'Delivered',
-        'Jan. 6, 2023, 3:24 p.m.',
-        'Jan. 8, 2023, 9:52 p.m.',
-        'Jan. 13, 2023, 6 p.m.',
-        'Jan. 11, 2023, 1:12 p.m.',
-        '2',
-    ]);
 
     const columns = [
         'Shipping Provider',
@@ -43,9 +67,9 @@ export default function Table() {
     const parentRef = useRef(null)
 
     const rowVirtualizer = useVirtualizer({
-        count: data.length,
+        count: shippingList.length,
         getScrollElement: () => parentRef.current,
-        estimateSize: () => 65,
+        estimateSize: () => 80,
         overscan: 5,
     })
 
@@ -86,7 +110,7 @@ export default function Table() {
                                         top: 0,
                                         left: 0,
                                         width: `110px`,
-                                        height: `65px`,
+                                        height: `80px`,
                                         padding: `0.5rem`,
                                         fontWeight: virtualRow.index === 0 ? 'bold' : 'normal',
                                         borderRight: `1px solid #ececec`,
@@ -100,7 +124,7 @@ export default function Table() {
                                         virtualRow.index === 0
                                             ? columns[virtualColumn.index]
                                             : virtualColumn.index !== 11
-                                                ? data[virtualRow.index][virtualColumn.index]
+                                                ? shippingList[virtualRow.index][virtualColumn.index]
                                                 : <Button fontSize="xs" p={2} py={2} h={`28px`} onClick={() => showShipmentDetails(virtualRow.index)}>Show</Button>
                                     }
                                 </div>
