49232055db5f57508a4923bff6e9efa7b46434a6
Raghav <raghav.kanwal@unicommerce.com>
2023-02-03 18:57:09 +0530

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,21 +11,20 @@ export default function Home() {
   const [duration, setDuration] = useState<Duration>(Duration.LAST_WEEK);
   const [fromDate, setFromDate] = useState<string>(new Date().toISOString().split('T')[0]);
   const [toDate, setToDate] = useState<string>(new Date().toISOString().split('T')[0]);
-  const [moreFilters, setMoreFilters] = useState([]);
 
   const options = [{
     label: "Delivered",
     value: "delivered"
-  },{
+  }, {
     label: "Sent",
     value: "sent"
-  },{
+  }, {
     label: "Masked",
     value: "masked"
-  },{
+  }, {
     label: "En Route",
     value: "enroute"
-  },{
+  }, {
     label: "Testing",
     value: "testing"
   }];
@@ -37,41 +36,44 @@ export default function Home() {
       <div className="row">
         <div className="col-md-12">
           <Card className="mb-4">
-            <CardHeader bg="gray.100" py={2}>
+            <CardHeader bg="gray.100" py={4}>
               <Flex justifyContent="space-between" align="center">
-                <Input size="sm" value={searchQuery} placeholder="Search AWB/Order number/Phone number" w={`15%`} bg={`#fff`} onChange={(e) => setSearchQuery(e.target.value)} />
-                <Flex justifyContent="flex-end" align={`center`}>
-                  <Text as="span" mr={2}>Timeline: </Text>
-                  <Menu>
-                    <MenuButton as={Button} bg={`white`} rightIcon={<AiFillCaretDown />} w="8.5rem" h={`2rem`} p={2} fontSize="sm">
-                      {duration}
-                    </MenuButton>
-                    <MenuList>
-                      {Object.keys(Duration).map((key, index) => (
-                        <MenuItem key={index} onClick={() => setDuration(Duration[key as keyof typeof Duration])}>
-                          {Duration[key as keyof typeof Duration]}
-                        </MenuItem>
-                      ))}
-                    </MenuList>
-                  </Menu>
-                  {
-                    duration === Duration.CUSTOM
-                      ? (
-                        <>
-                          <Box ml={2}>
-                            <Text as="span" mr={2}>From: </Text>
-                            <Input type="date" value={fromDate} onChange={(e) => setFromDate(e.target.value)} w="10rem" background="white" />
-                          </Box>
-                          <Box ml={2}>
-                            <Text as="span" mr={2}>To: </Text>
-                            <Input type="date" value={toDate} onChange={(e) => setToDate(e.target.value)} w="10rem" background="white" />
-                          </Box>
-                        </>
-                      )
-                      : null
-                  }
+                <Input value={searchQuery} placeholder="Search AWB/Order number/Phone number" w={`30%`} bg={`#fff`} onChange={(e) => setSearchQuery(e.target.value)} />
+                <Flex gap={4}>
+
+                  <Flex justifyContent="flex-end" align={`center`}>
+                    <Text as="span" mr={2}>Timeline: </Text>
+                    <Menu>
+                      <MenuButton as={Button} bg={`white`} rightIcon={<AiFillCaretDown />} w="8.5rem" h={`2rem`} p={2} fontSize="sm">
+                        {duration}
+                      </MenuButton>
+                      <MenuList>
+                        {Object.keys(Duration).map((key, index) => (
+                          <MenuItem key={index} onClick={() => setDuration(Duration[key as keyof typeof Duration])}>
+                            {Duration[key as keyof typeof Duration]}
+                          </MenuItem>
+                        ))}
+                      </MenuList>
+                    </Menu>
+                    {
+                      duration === Duration.CUSTOM
+                        ? (
+                          <>
+                            <Box ml={2}>
+                              <Text as="span" mr={2}>From: </Text>
+                              <Input type="date" value={fromDate} onChange={(e) => setFromDate(e.target.value)} w="10rem" background="white" />
+                            </Box>
+                            <Box ml={2}>
+                              <Text as="span" mr={2}>To: </Text>
+                              <Input type="date" value={toDate} onChange={(e) => setToDate(e.target.value)} w="10rem" background="white" />
+                            </Box>
+                          </>
+                        )
+                        : null
+                    }
+                  </Flex>
+                  <Button colorScheme="teal" size="sm">Search</Button>
                 </Flex>
-                <Button colorScheme="teal" size="sm">Search</Button>
               </Flex>
             </CardHeader>
             <CardBody className="px-0 py-0" bg="white">
