20a4bb590fd0e020c203a0cdb2af3a848f7270b1
Raghav <raghav.kanwal@unicommerce.com>
2023-02-08 15:24:53 +0530

    Added UI-Demo Page

---
+++ src/pages/ui-demo/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from '@chakra-ui/react';
+import AdminLayout from '@layout/AdminLayout/AdminLayout';
+
+export default function UIDemo() {
+    return (
+        <AdminLayout>
+        <Text>UI-Demo Page</Text>
+        </AdminLayout>
+    )
+}
