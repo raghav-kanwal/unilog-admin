60326e017a333b40af2a7e6bb91881def7eafef7
Raghav <raghav.kanwal@unicommerce.com>
2023-02-08 16:04:24 +0530

    Datatable basic implements

--- src/pages/ui-demo/index.tsx
+++ src/pages/ui-demo/index.tsx
@@ -1,10 +1,12 @@
 import { Text } from '@chakra-ui/react';
 import AdminLayout from '@layout/AdminLayout/AdminLayout';
+import Datatable from 'src/components/datatable/datatable.component';
 
 export default function UIDemo() {
     return (
         <AdminLayout>
         <Text>UI-Demo Page</Text>
+        <Datatable />
         </AdminLayout>
     )
 }
